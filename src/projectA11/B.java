d66345c0f49ab0873d7afcbf8bba43208ddd79f3
OrengYee <109725771+OrengYee@users.noreply.github.com>
2022-08-18 16:36:57 +0900

    Add files via upload

---
+++ src/projectA11/B.java
@@ -0,0 +1,12 @@
+package projectA11;
+
+public class B {
+	public B() {
+		System.out.println("생성자 B()");
+	}
+	public B(int x) {
+		
+		System.out.println("매개변수 생성자 B(int x)");
+	}
+
+}
---
+++ src/projectA11/Child.java
@@ -0,0 +1,17 @@
+package projectA11;
+
+public class Child extends Parent{
+	
+	@Override
+	public void method2() {
+		System.out.println("Child-method2()");
+		
+	}
+	
+public void method3() {
+	System.out.println("Child-method3()");
+}
+
+
+
+}
