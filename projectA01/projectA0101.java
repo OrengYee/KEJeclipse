13ce69777dd56c49b7378e2522e6d1d5a23f291e
OrengYee <109725771+OrengYee@users.noreply.github.com>
2022-08-18 16:33:58 +0900

    Add files via upload

---
+++ projectA01/projectA0101.java
@@ -0,0 +1,19 @@
+package projectA01;
+
+public class projectA0101 {
+	
+	/* https://blog.naver.com/choiime 강사님 블로그 
+	 * choiime@gmail.com
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요. 만나서 반가워요.");
+		System.out.println(" ");
+		System.out.println("뿡");
+		
+
+	}
+
+}
