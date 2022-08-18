79144995f68c935c570326ab1f2773a2a1adc722
OrengYee <109725771+OrengYee@users.noreply.github.com>
2022-08-18 16:28:36 +0900

    3

---
+++ projectA03/projectA0310.java
@@ -0,0 +1,17 @@
+package projectA03;
+
+public class projectA0310 {
+
+	public static void main(String[] args) {
+		boolean b1;
+		
+		b1 = (10>1);
+		
+		int ia1;
+		
+		b1 =(10>1);
+	
+
+	}
+
+}
---
+++ projectA03/projectA0311.java
@@ -0,0 +1,14 @@
+package projectA03;
+
+public class projectA0311 {
+
+	public static void main(String[] args) {
+	Integer ia = 10;
+	
+	System.out.println("안녕하세요");
+	
+	
+
+	}
+
+}
